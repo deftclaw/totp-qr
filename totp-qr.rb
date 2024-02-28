bd5bfb50310ee681b9871809b2a4eb3a6af54974
deftclaw <eeccher@cppwind.com>
2024-02-28 10:33:31 -0700

    Loop sharing totps from auth

--- totp-qr.rb
+++ totp-qr.rb
@@ -1,21 +1,48 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'auth'
 require 'rqrcode'
 require 'highline/import'
 
-EMAIL = HighLine.new.ask("Email: ").gsub(/@/, '%40')
-ISSUER = HighLine.new.ask("Account with? (eg. Amazon, Google, GitHub...): ")
-SECRET = ask("TOTP Secret: ") { |inp| inp.echo = '*' }
+@tleaf = `mktemp`.chomp!
+@link  = "#{ENV['HOME']}/.config/otp.yml"
+@rute  = "#{ENV['TMP']}/totp"
 
-message = "otpauth://totp/#{EMAIL}?issuer = #{ISSUER}&secret = #{SECRET}"
-tleaf   = `mktemp`.chomp!
+@ld = {
+  link: ->(l) { system("ln -sf #{@rute}/#{l} #{@link}") },
+  list: -> { Dir.new('.').children.select{|l| l.end_with? '.yml'} },
+  message: ->(p) { "otpauth://totp/#{p[:email]}?issuer=#{p[:issuer]}&secret=#{p[:secret]}" },
+  props: ->(s) {{ email: ask('Email/Username: ').gsub!(/@/, '%40'), issuer: ask('Company/Account Issuer: '), secret: s }},
+  qr: ->(msg) { RQRCode::QRCode.new(msg).as_png(size: 150) },
+  secret: -> { temp = Auth::OTP.new ; temp::to_yaml.split(/\n/)[-1].split(/: /)[-1] }
+}
 
-File.write(tleaf, RQRCode::QRCode.new(message).as_png(size: 300), mode: 'wb')
-begin
-  `feh #{tleaf}`
-rescue => Interrupt
-  p 'User Cancelled'
-ensure
-  File.delete(tleaf)
+def share_totp(leaf)
+  puts "Reading: #{leaf}"
+  @ld[:link].call(leaf)
+
+  File.write(
+    @tleaf,
+    @ld[:qr].call(
+      @ld[:message].call(
+        @ld[:props].call(
+          @ld[:secret].call
+    ))),
+    mode: 'wb'
+  )
+
+  begin
+    `feh #{@tleaf}`
+  rescue Interrupt => error
+    puts('User Cancelled')
+  ensure
+    File.delete(@tleaf)
+  end
+end
+
+def loop_totps(list)
+  list.each do |leaf|
+    share_totp(leaf)
+  end
 end
